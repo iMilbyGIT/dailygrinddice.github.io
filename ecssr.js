575096bd00a38f327b0d98f5d4b0d16535a99b59
Adam Milbrath <adammilbrath@gmail.com>
2019-11-11 15:57:13 -0600

    not init, had to remake my repo. have added a landing/home page and created 6 dice functions with different side values

---
+++ ecssr.js
@@ -0,0 +1,11 @@
+"use strict";
+alert("Welcome to the most frustra.. I mean fun, single-day simulation of Mada's life! Throw your free-will aside for this short and funny game/story that follows a nerd looking to level up his coding/developer skills to over 9000. Before mastering his skills and landing a job, he must first master his Daily Grind, typically consisting of waking up, eating, coding, shitting, sleeping, and repeating. Please click ok to continue and see if you can make it through Mada's Daily Grind!")
+let dice4 = Math.round(Math.random() * 4)
+let dice6 = Math.round(Math.random() * 6)
+let dice8 = Math.round(Math.random() * 8)
+let dice10 = Math.round(Math.random() * 10)
+let dice12 = Math.round(Math.random() * 12)
+let dice20 = Math.round(Math.random() * 20)
+
+
+console.log(dice4);
